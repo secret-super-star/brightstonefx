1924271705759fa8ae8ac0395716a1e8d4d02cbf
secret-super-star <mjone352@gmail.com>
2020-11-28 21:47:59 +0800

    add slider page

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,6 +16,7 @@ import {
   View,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
 
 import {
@@ -27,19 +28,58 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 import { WebView } from 'react-native-webview';
-
+import AppIntroSlider from 'react-native-app-intro-slider';
 
 declare const global: { HermesInternal: null | {} };
 
 const App = () => {
+  const [showRealApp, setShowRealApp] = useState(false);
+
+  const onDone = () => {
+    setShowRealApp(true);
+  };
+  const onSkip = () => {
+    setShowRealApp(true);
+  };
+
+  const RenderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingBottom: 100,
+        }}>
+        <Text style={styles.introTitleStyle}>
+          {item.title}
+        </Text>
+        <Image
+          style={styles.introImageStyle}
+          source={item.image} />
+        <Text style={styles.introTextStyle}>
+          {item.text}
+        </Text>
+      </View>
+    );
+  };
+
   return (
     <>
-      
-      <View style={styles.body}>
-        <WebView
-          source={{ uri: 'https://login.brightstonefx.com/login' }}
-        />
-      </View>
+      {showRealApp ? (
+        <View style={styles.body}>
+          <WebView
+            source={{ uri: 'https://login.brightstonefx.com/login' }}
+          />
+        </View>
+      ) : (
+          <AppIntroSlider
+            data={slides}
+            renderItem={RenderItem}
+            onDone={onDone}
+          />
+        )}
     </>
   );
 };
@@ -49,6 +89,57 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
     flex: 1,
   },
+  introImageStyle: {
+    width: 200,
+    height: 200,
+  },
+  introTextStyle: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 30,
+  },
+  introTitleStyle: {
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 16,
+    fontWeight: 'bold',
+  },
 });
+
+const slides = [
+  {
+    key: 's4',
+    title: 'Best Deals',
+    text: ' Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_best_deals.png',
+    },
+    backgroundColor: '#3395ff',
+  },
+  {
+    key: 's5',
+    title: 'Bus Booking',
+    text: 'Enjoy Travelling on Bus with flat 100% off',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_bus_ticket_booking.png',
+    },
+    backgroundColor: '#f6437b',
+  },
+  {
+    key: 's3',
+    title: 'Great Offers',
+    text: 'Enjoy Great offers on our all services',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_discount.png',
+    },
+    backgroundColor: '#22bcb5',
+  },
+
+];
 
 export default App;
