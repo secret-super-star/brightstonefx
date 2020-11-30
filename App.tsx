4d5efe964dd2caa7538879d8400202da86f34fe3
secret-super-star <mjone352@gmail.com>
2020-11-30 14:49:37 +0800

    slider background image

--- App.tsx
+++ App.tsx
@@ -16,7 +16,8 @@ import {
   View,
   Text,
   StatusBar,
-  Image
+  Image,
+  ImageBackground
 } from 'react-native';
 
 import {
@@ -47,20 +48,12 @@ const App = () => {
       <View
         style={{
           flex: 1,
-          backgroundColor: item.backgroundColor,
           alignItems: 'center',
           justifyContent: 'space-around',
-          paddingBottom: 100,
         }}>
-        <Text style={styles.introTitleStyle}>
-          {item.title}
-        </Text>
-        <Image
-          style={styles.introImageStyle}
-          source={item.image} />
-        <Text style={styles.introTextStyle}>
-          {item.text}
-        </Text>
+        <ImageBackground source={item.image.uri} style={styles.image}>
+        </ImageBackground>
+
       </View>
     );
   };
@@ -88,56 +81,38 @@ const styles = StyleSheet.create({
   body: {
     backgroundColor: Colors.white,
     flex: 1,
+    flexDirection: "column"
   },
-  introImageStyle: {
-    width: 200,
-    height: 200,
-  },
-  introTextStyle: {
-    fontSize: 18,
-    color: 'white',
-    textAlign: 'center',
-    paddingVertical: 30,
-  },
-  introTitleStyle: {
-    fontSize: 25,
-    color: 'white',
-    textAlign: 'center',
-    marginBottom: 16,
-    fontWeight: 'bold',
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: '100%',
+    height: '100%'
   },
 });
 
 const slides = [
   {
-    key: 's4',
-    title: 'Best Deals',
-    text: ' Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services Best Deals on all our services',
+    key: 's1',
     image: {
       uri:
-        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_best_deals.png',
+        require('./assets/1.jpg'),
     },
-    backgroundColor: '#3395ff',
   },
   {
-    key: 's5',
-    title: 'Bus Booking',
-    text: 'Enjoy Travelling on Bus with flat 100% off',
+    key: 's2',
     image: {
       uri:
-        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_bus_ticket_booking.png',
+        require('./assets/2.jpg'),
     },
-    backgroundColor: '#f6437b',
   },
   {
     key: 's3',
-    title: 'Great Offers',
-    text: 'Enjoy Great offers on our all services',
     image: {
       uri:
-        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_discount.png',
+        require('./assets/3.jpg'),
     },
-    backgroundColor: '#22bcb5',
   },
 
 ];
